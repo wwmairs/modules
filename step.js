7d9bb7edc35c3e6249c4bbf8062424a3d4067ab9
wwmairs <wwmairs@gmail.com>
2018-05-28 17:29:32 -0400

    changed paradigm, a step is not a physical thing, a slider is the interface for it

---
+++ step.js
@@ -0,0 +1,39 @@
+/**
+  * step.js
+  *
+  * a sequencer step
+  * contains its own oscillator, vca, and envelope
+  *
+  * connects to a context
+  * oscillator can be connected to for FM
+  * envelope can be connected to for gate
+  *
+  * depends on modules:
+  * 	vco.js
+  * 	vca.js
+  * 	adsr.js
+  **/
+
+class STEP {
+	constructor(_ctx, _cont) {
+		this.ctx  =  _ctx;
+		this.cont = _cont;
+		
+		this.vco = new  VCO(this.ctx);
+		this.vca = new  VCA(this.ctx);
+		this.env = new ADSR(this.ctx);
+		this.frequencyParam = this.vco.frequencyParam;
+
+		// connecting things up	
+		this.vco.connect(this.vca);
+		this.env.connect(this.vca.amplitudeParam);
+	  // vca needs to be connected to something
+
+	  }
+
+	gateOn() {
+  }
+
+	gateOn(duration) {
+	}
+}
