036d85bec13017d7b488d1da4285487ea9ae5665
wwmairs <wwmairs@gmail.com>
2018-05-28 18:54:19 -0400

    touched sequ.js, a self contained sequencer

---
+++ sequ.js
@@ -0,0 +1,18 @@
+/**
+  * sequ.js
+  *
+  * a sequencer 
+  * contains its own steps
+  *
+  *
+  * depends on modules:
+  * 	vco.js
+  * 	vca.js
+  * 	adsr.js
+  * 	step.js
+  **/
+
+class SEQU {
+	constructor() {
+	}
+}
