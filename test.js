75cda81abf58acc5e7408661906a18f666524912
wwmairs <wwmairs@gmail.com>
2018-05-29 11:59:58 -0400

    took out the unimportant stuff

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
 var context = new (window.AudioContext || window.webkitAudioContext);
 
-var cont = document.getElementById("step-container");
-
-let slide = document.createElement("vertical-slider");
-cont.appendChild(slide);
-console.log(slide);
 
