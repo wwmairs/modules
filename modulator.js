35417caeee8cda702e9e2882605facdba428c8cd
wwmairs <wwmairs@gmail.com>
2018-06-05 19:00:46 -0400

    modulator.js is useful afterall, since it needs to have a mod frequency and vca

---
+++ modulator.js
@@ -0,0 +1,54 @@
+/**
+	* a modulator module that modulates
+	* 
+	* relies on vco.js and vca.js
+	**/
+
+class MODULATOR {
+    constructor(context) {
+        this.context         = context;
+        this.modulator       = new VCO(this.context);
+				this.vca						 = new VCA(this.context);
+        this.frequencyParam  = this.modulator.frequencyParam;
+        this.frequency       = 146;
+			  this.modulator.connect(this.vca);	
+
+        this.input  = this.modulator;
+        this.output = this.vca;
+    }
+
+    set waveForm(w) {
+        // should assert that w is well-formed
+        this.modulator.waveForm = w;
+    }
+
+    get waveForm() {
+        return this.modulator.waveForm;
+    }
+
+		set amplitude(a) {
+				this.vca.amplitude(a);
+		}
+	
+		get amplitude() {
+				return this.vca.amplitude;
+		}
+
+    set frequency(f) {
+        this.frequencyParam.setValueAtTime(f, this.context.currentTime);
+    }
+
+    get frequency() {
+        return this.frequencyParam.value;
+    }
+
+    // this can connect to either another module, or an AudioNode
+    // I wonder how to let it connect to an AudioParam as well
+    connect(module) {
+        if (module.hasOwnProperty('input')) {
+            this.output.connect(module.input);
+        } else {
+            this.output.connect(module);
+        }
+    }
+}
