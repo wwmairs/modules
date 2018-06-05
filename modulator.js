767352d3cc560e696c83d4e6ba6627c6b8860ca2
wwmairs <wwmairs@gmail.com>
2018-06-05 18:30:54 -0400

    skeleton for modulator

---
+++ modulator.js
@@ -0,0 +1,14 @@
+/**
+  * modulator.js
+	*
+	* a frequency modulator module
+	* it's just got one oscillator so far
+	* definitely needs some refinement
+	*
+	**/
+
+class MODULATOR {
+	constructor(context) {
+		this
+	}
+}
