87a418b73b7a7c2a1e3cff642815ccdab382357d
wwmairs <wwmairs@gmail.com>
2018-07-10 14:28:57 -0300

    made INST a little more abstract, to support MONO, and POLY subclasses

--- inst.js
+++ inst.js
@@ -2,6 +2,8 @@
   * inst.js
   *
   * an instrument super class
+	* 
+	* and a monophonic sub class
   * contains its own oscillator, vca, and envelope
   *
   * connects to a context
@@ -14,29 +16,36 @@
   * 	adsr.js
   **/
 
-class INST {
+
+class MONO extends INST {
 	constructor(_ctx) {
-		this.ctx =  _ctx;
-		this.vco = new  VCO(this.ctx);
-		this.vca = new  VCA(this.ctx);
-		this.env = new ADSR(this.ctx);
+		super(_ctx);
+		this.vco    = new  VCO(this.ctx);
+		this.vca    = new  VCA(this.ctx);
+		this.env    = new ADSR(this.ctx);
 		this.input 	= this.vca;
 		this.output = this.vca;
-		this.duration 			= 250;
 		this.frequencyParam = this.vco.frequencyParam;
 
 		// connecting things up	
 		this.vco.connect(this.vca);
 		this.env.connect(this.vca.amplitudeParam);
 	  // vca needs to be connected to something
-
-	  }
+	}
 
 	set frequency(f) {
 		this.vco.frequency = f;
 	}
 
 	gateOn() {
+		this.env.gateOn();
+	}
+
+	gateOn(f) {
+		this.env.gateOn();
+	}
+
+	gateOn(f, d) {
 		this.env.gateOn();
 	}
 
@@ -47,4 +56,32 @@ class INST {
 			this.output.connect(module);
 		}
 	}
+}
+
+class INST {
+	constructor(_ctx) {
+		this.ctx      =  _ctx;
+		this.duration = 250;
+	  }
+	
+	set frequency(f) {
+		throw new Error("getter 'frequency' must be defined in a subclass of INST");
+	}
+
+	gateOn() {
+		throw new Error("method 'gateOn()' must be defined in a subclass of INST");
+	}
+
+	gateOn(f) {
+		throw new Error("method 'gateOn(f)' must be defined in a subclass of INST");
+	}
+
+	gateOn(f, d) {
+		throw new Error("method 'gateOn(f, d)' must be defined in a subclass of INST");
+	}
+	
+	connect(module) {
+		throw new Error("method 'connect(module)' must be defined in a subclass of INST");
+	}
+
 }
