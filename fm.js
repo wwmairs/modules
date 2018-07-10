767ae25ec79bd5b69588e35a871f0bc5ff77f2fc
wwmairs <wwmairs@gmail.com>
2018-07-10 14:14:34 -0300

    shell of fm.js

---
+++ fm.js
@@ -0,0 +1,18 @@
+/**
+  * fm.js
+	*
+	* an FM synthesis instrument
+	* sub class of INST
+	*
+	* depends on modules:
+	* 	inst.js
+	*		modu.js
+	*	and their dependencies
+	*
+	**/
+
+class FM extends INST {
+	constructor(_ctx) {
+		super(_ctx);
+	}
+}
