d0a758fea80cedd0997a37da6f21445d4cbe78f1
wwmairs <wwmairs@gmail.com>
2018-07-16 14:54:04 -0300

    sketch for a few commands

--- worker.js
+++ worker.js
@@ -5,23 +5,52 @@
 	* hold onto the context and create and store modules
 	*
 	**/
+import "modules.js";
 
 var ctx = new (window.AudioContext || window.webkitAudioContext);
-var test_modules = [];
+var instruments = [];
 
 onconnect = function(e) {
 	var port = e.ports[0];
 
 	port.onmessage = function(e) {
-		test_modules.push(e.data);
-		console.log('test_modules', test_modules);
+		/** accepts commands:
+			* 'new <instrument type>'
+			* 'noteon <instrument name>'
+			* 'noteon <instrument name> duration'
+			* 'noteon <instrument name> duration frequency'
+			**/
+	  let cmd = e.data[0].split(" ");	
+		switch(cmd[0]) {
+			case 'new':
+				/** accepts types:
+				  * 'FM'
+					**/
+				switch(cmd[1]) {
+					case 'FM':
+						let newFM = new FM(ctx);
+						let name  = "FM" + instruments.length;
+						instruments.push({"name" : name, "instrument" : newFM});
+						port.postMessage("new FM " + name);
+					default:
+						throw new Error("Unrecognized intstrument name:", cmd[1]);
+				}
+			case 'noteon':
+				switch(cmd.length) {
+					// 'noteon <instrument name>'
+					case 2:
+					// 'noteon <instrument name> duration'
+					case 3:
+					// 'noteon <instrument name> duration frequency'
+					case 4:
+					default:
+						throw new Error("Unrecognized noteon command:", e.data[0]);
+				}
+			default:
+				throw new Error("Unrecognized command:", e.data[0]);
+		}
 		/* messages the worker must respond to
-		 * 
-		 * 'new instrument'
-		 * 'new sequencer'
-		 * 'start sequencer'
-		 * 'conenct sequencer to instrument'
-		 * ''
+		 * a method, possibly with args
 		 */
 		//var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
 		//port.postMessage(workerResult);
