f75f7f9c9bf62efc24f7c565d0474882c6fa4821
wwmairs <wwmairs@gmail.com>
2018-07-18 18:15:44 -0300

    took out assert(b)

--- modules.js
+++ modules.js
@@ -19,12 +19,6 @@ function midiToFrequency(n) {
 	return MIDI_FREQS[n];
 }
 
-function assert(bool) {
-	if (!bool) {
-		console.log("failed assertion");
-		throw new Error("failed assertion");
-	}
-}
 
 function assert(bool, msg) {
 	if (!bool) {
